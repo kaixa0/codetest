bae96343479b1e08b9685f6138b7caf09757462e
kaixa0 <kaixa8903@gmail.com>
2026-09-11 14:34:58 +0800

    本地第三次提交，本地增加的第三行。

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -20,6 +20,7 @@ public class Main {
         System.out.printf("本地增加的第二行");
 
         System.out.printf("在线增加的第二行");
+        System.out.printf("本地增加的第三行");
         System.out.printf("在线增加的第三行");
     }
 }
